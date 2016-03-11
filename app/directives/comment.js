1b56b046356ea47ae0549d3f432ceaf4ed759e72
kilowatt11 <kilowatt11@cableone.net>
2016-03-10 17:28:27 -0700

    initialized the comment directory

---
+++ app/directives/comment.js
@@ -0,0 +1,8 @@
+angular.module('app').directive('answerComment', function(){
+    // return{
+    //     template:'<div>Answer: {{a.text}}</div>'
+    // }
+    return {
+        templateUrl: 'Partials/View2.html'
+    }
+})
