b8bf01c2ae58d2e65502647458dc5c67c93f4caf
kilowatt11 <kilowatt11@cableone.net>
2016-03-10 17:09:56 -0700

    fixed directives. http server now working with partials

--- app/directives/answer.js
+++ app/directives/answer.js
@@ -1,4 +1,4 @@
-app.directive('questionAnswer', function(){
+angular.module('app').directive('questionAnswer', function(){
     // return{
     //     template:'<div>Answer: {{a.text}}</div>'
     // }
